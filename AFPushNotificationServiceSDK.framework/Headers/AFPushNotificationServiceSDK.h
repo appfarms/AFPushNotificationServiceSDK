67787fe962531c0f57e8d09bbfe15bcf73441b25
Daniel Kuhnke <d.kuhnke@appfarms.com>
2015-12-21 15:51:06 +0100

    Initial

--- AFPushNotificationServiceSDK.framework/Headers/AFPushNotificationServiceSDK.h
+++ AFPushNotificationServiceSDK.framework/Headers/AFPushNotificationServiceSDK.h
@@ -2,7 +2,7 @@
 //  AFPushNotificationServiceSDK.h
 //  AFPushNotificationServiceSDK
 //
-//  Created by Daniel Kuhnke on 20.11.15.
+//  Created by Daniel Kuhnke on 21.12.15.
 //  Copyright © 2015 appfarms GmbH & Co. KG. All rights reserved.
 //
 
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double AFPushNotificationServiceSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char AFPushNotificationServiceSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AFPushNotificationServiceSDK/PublicHeader.h>
-
 #import <AFPushNotificationServiceSDK/AFPushManager.h>
+
